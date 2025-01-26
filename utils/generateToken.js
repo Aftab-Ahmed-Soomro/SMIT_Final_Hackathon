99494c3f11c1d5d2bea8812901c62e3cd661ce09
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2025-01-26 06:22:12 +0500

    updated

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, "a1B2c3D4e5F6g7H8i9J0kL1m2N3oP4Q5r6S7t8U9V0", { expiresIn: '30d' });
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
 export default generateToken;
